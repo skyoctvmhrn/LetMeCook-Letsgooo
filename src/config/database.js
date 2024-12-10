2e3e953d975bb333f15f96818e80539fcea58675
Saskya Octavia <saskyaoctavia@gmail.com>
2024-12-10 11:51:20 +0700

    update database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,  
-    dialect: "mysql"
+const sequelize = new Sequelize("letmecook", "letmecook", "letmecook", {
+    host: "34.34.219.15",
+    dialect: "mysql",
+    port: 3306 // Port default MySQL
 });
 
 module.exports = sequelize;
