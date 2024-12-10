e06aad48446b3a5de24b67a833769295581e6006
Isa <isaabqari@gmail.com>
2024-12-10 12:52:52 +0700

    commit config tambahan

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,14 @@ dotenv.config();
 const sequelize = new Sequelize("letmecook", "letmecook", "letmecook", {
     host: "34.34.219.15",
     dialect: "mysql",
-    port: 3306 // Port default MySQL
+    port: 3306, // Port default MySQL
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Bisa diatur ke `true` jika ada CA Certificate
+        }
+    },
+    logging: console.log
 });
 
 module.exports = sequelize;
