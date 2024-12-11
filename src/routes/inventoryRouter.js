4efa603f7df85cf9ea64c52bb25517919aaa29dc
Saskya Octavia <121883411+skyoctvmhrn@users.noreply.github.com>
2024-12-11 10:28:51 +0700

    Update inventoryRouter.js

--- src/routes/inventoryRouter.js
+++ src/routes/inventoryRouter.js
@@ -1,20 +1,30 @@
-"use strict";  //biar lebih aman
-const express = require('express');
-const {getAllInventory, destroy}=require('../controller/inventoryController.js')
-const {updateManual}=require('../controller/inventoryController.js')
-const {searchInventoryByIngredient}=require('../controller/inventoryController.js')
-const {getInventoryById}=require('../controller/inventoryController.js')
-const {deleteInventory}=require('../controller/inventoryController.js')
+"use strict"; //biar lebih aman
+const express = require("express");
+const {
+  getAllInventory,
+  destroy,
+} = require("../controller/inventoryController.js");
+const { updateManual } = require("../controller/inventoryController.js");
+const {
+  searchInventoryByIngredient,
+} = require("../controller/inventoryController.js");
+const { getInventoryById } = require("../controller/inventoryController.js");
+const { deleteInventory } = require("../controller/inventoryController.js");
+const { store } = require("../controller/inventoryController.js");
+
 const inventoryRouter = express.Router();
 
 //Tampilin semua inventory
-inventoryRouter.get('/inventories/:user_id', getAllInventory); //ini user harus kirim data berupa json
+inventoryRouter.get("/inventories/:user_id", getAllInventory); //ini user harus kirim data berupa json
 //pake put untuk update bahan :)
-inventoryRouter.put('/update-inventory/:id', updateManual);
+inventoryRouter.put("/update-inventory/:id", updateManual);
 //Cari inventory berdasarkan nama bahan
-inventoryRouter.get('/search-inventory',searchInventoryByIngredient);
+inventoryRouter.get("/search-inventory", searchInventoryByIngredient);
 // Route untuk mengambil inventory berdasarkan ID
-inventoryRouter.get('/inventory/:id', getInventoryById); 
+inventoryRouter.get("/inventory/:id", getInventoryById);
 //Route untuk hapus >> klo mau hapus panggil .deleteaja (buat methodnya)
-inventoryRouter.delete('/delete-inventory/:id', deleteInventory);
-module.exports=inventoryRouter;
+inventoryRouter.delete("/delete-inventory/:id", deleteInventory);
+//membuat inventory
+inventoryRouter.post("/inventory", store);
+
+module.exports = inventoryRouter;
